208476567ea5114e0c83362526fdafddc0545cd8
Kenny Luque <kenny.luque.t@uni.pe>
2022-03-01 16:43:29 -0500

    commit2

--- User.py
+++ User.py
@@ -2,17 +2,22 @@ class User:
     balance=8000
     def __init__(self, nombre):
         self.nombre=nombre
+        return self
         
         
     def hacer_deposito(self, monto):
         self.balance+=monto
+        return self
     
     def hacer_retiro(self, monto):
-        self.balance-=monto    
+        self.balance-=monto
+        return self    
     
     def mostrar_balance_usuario(self):
         print(f"Usuario:{self.nombre}, Balance:{self.balance}")
+        return self
         
     def transf_dinero(self, otro_usuario,monto):
         self.balance-=monto
         otro_usuario.balance +=monto
+        return self
--- main.py
+++ main.py
@@ -5,25 +5,16 @@ Julián = User("Julián")
 Paul = User("Paul")
 Rebeca = User("Rebeca")
 
-Julián.hacer_deposito(100)
-Julián.hacer_deposito(100)
-Julián.hacer_deposito(100)
-Julián.hacer_retiro(100)
-Julián.mostrar_balance_usuario()
+Julián.hacer_deposito(100).hacer_deposito(100).hacer_deposito(100).hacer_retiro(100).mostrar_balance_usuario()
 
-Paul.hacer_deposito(100)
-Paul.hacer_deposito(100)
-Paul.hacer_retiro(100)
-Paul.hacer_retiro(100)
-Paul.mostrar_balance_usuario()
 
-Rebeca.hacer_deposito(100)
-Rebeca.hacer_retiro(100)
-Rebeca.hacer_retiro(100)
-Rebeca.hacer_retiro(100)
-Rebeca.mostrar_balance_usuario()
+Paul.hacer_deposito(100).hacer_deposito(100).hacer_retiro(100).hacer_retiro(100).mostrar_balance_usuario()
 
-Julián.transf_dinero(Rebeca,100)
-Julián.mostrar_balance_usuario()
+
+Rebeca.hacer_deposito(100).hacer_retiro(100).hacer_retiro(100).hacer_retiro(100).mostrar_balance_usuario()
+
+
+Julián.transf_dinero(Rebeca,100).mostrar_balance_usuario()
+
 Rebeca.mostrar_balance_usuario()
 
